75aa5cdb2fd3f91dce7e52d0131e48d20a328dfc
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2023-05-11 03:36:48 +0600

    mistake

---
+++ basic/day 11/string.py
@@ -0,0 +1,21 @@
+""" 
+        1) data type 
+
+        2) ordered 
+
+        3) immutable
+
+        4) 3 type decleration 
+                a = 'hello mf'
+                b = "hello mf"
+                c = '''hello mf'''
+
+ """
+
+a = 'hello mf'
+b = "hello mf"
+c = '''hello mf'''
+
+print(a)
+print(b)
+print(c)
